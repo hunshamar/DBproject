46fba913d28c737c16af936ea836f2851f995a27
Asgeir Hunshamar <asgeirhuns@gmail.com>
2019-03-21 15:19:14 +0100

    Laget print for alle tabeller

--- projectDB/Driver.java
+++ projectDB/Driver.java
@@ -9,7 +9,7 @@ public class Driver {
 
 	
 	public static final String db_name = "db_project_workout";
-	public static final String url = "jdbc:mysql://localhost:3306/" +"exerciseProgram" +  "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public static final String url = "jdbc:mysql://localhost:3306/" +"workoutProgram" +  "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	public static final String username = "root";
 	public static final String password = "password";
 	
